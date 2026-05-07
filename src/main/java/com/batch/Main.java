113cac119f32022a8e8fb7c963fc389b4058d5b1
kcorvaisier <kcorvaisier@dedicast.fr>
2026-05-07 22:31:21 +0200

    first commit

---
+++ src/main/java/com/batch/Main.java
@@ -0,0 +1,18 @@
+package com.batch;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        log.info("=== CSV Batch Processor - Démarrage ===");
+        // TODO : initialiser la configuration
+        // TODO : initialiser le repository
+        // TODO : initialiser le service batch
+        // TODO : démarrer le watcher de fichiers
+        log.info("Application démarrée.");
+    }
+}
